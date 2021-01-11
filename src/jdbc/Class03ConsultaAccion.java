d1d95e025b472b31fb0220961ed7e241caa393a0
Usuario <Usuario@DESKTOP-H22GIVL.technicolor.net>
2021-01-11 15:35:05 +0100

    mommit 00

---
+++ src/jdbc/Class03ConsultaAccion.java
@@ -0,0 +1,12 @@
+package jdbc;
+
+import java.util.Scanner;
+
+public class Class03ConsultaAccion {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("introduzca instruccion de enfermo a eliminar");
+
+    }
+}
